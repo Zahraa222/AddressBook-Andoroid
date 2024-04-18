f96c687457ade4ceec02f3ed1dca13f2445f4aaa
Zahraa222 <zzmmk2003@gmail.com>
2024-04-18 12:11:02 -0400

    Added Edit favorites functionality

--- app/src/main/java/com/example/addressbook/MainActivity.java
+++ app/src/main/java/com/example/addressbook/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.addressbook;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -17,14 +21,18 @@ import java.lang.reflect.Array;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    BottomNavigationView nav;
-    ListView fave;
+    private BottomNavigationView nav;
+    private ListView fave;
+    private Button faveButton;
+   private AlertDialog.Builder favealert;
+   private int selectedPosition = -1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         fave = findViewById(R.id.faveList);
+        faveButton = findViewById(R.id.faveEdit);
 
         ContactManager manager = ContactManager.getContact();
         List<String> contacts = manager.getFavoritesList();
@@ -40,6 +48,39 @@ public class MainActivity extends AppCompatActivity {
             contacts.add(new_fave);
             adapter.notifyDataSetChanged(); // Update the adapter to reflect the new item
         }
+
+        fave.setOnItemClickListener((parent, view, position, id) -> {
+            selectedPosition = position;
+            faveButton.setVisibility(View.VISIBLE);
+        });
+        //-------------------------------------------------------
+        favealert = new AlertDialog.Builder(MainActivity.this);
+        favealert.setTitle("Edit Options");
+        favealert.setMessage("Choose Whether you would Like to remove Contacts from favorites or Cancel Action:");
+        favealert.setCancelable(false);
+        favealert.setPositiveButton("Cancel", (DialogInterface.OnClickListener) (dialog, which) ->{
+            dialog.dismiss();
+        });
+        favealert.setNegativeButton("Remove from favorites", (DialogInterface.OnClickListener) (dialog, which) ->{
+            if (selectedPosition != -1){
+                contacts.remove(selectedPosition);
+                adapter.notifyDataSetChanged();
+                dialog.dismiss();
+                faveButton.setVisibility(View.INVISIBLE);
+            }
+
+        });
+
+
+        faveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog alertDialog = favealert.create();
+                alertDialog.show();
+
+            }
+        });
+
 
         nav = findViewById(R.id.bottom_navigation);
         nav.setOnItemSelectedListener(item -> {
